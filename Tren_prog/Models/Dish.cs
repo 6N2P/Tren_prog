8a15c29e8d9f2ed518d968058398ced89c105907
6N2P <umnik1985@mail.ru>
2022-06-30 22:32:45 +0500

    Добавьте файлы проекта.

---
+++ Tren_prog/Models/Dish.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tren_prog.Models
+{
+    public class Dish
+    {
+        private int id;
+        private string name;
+    }
+}
